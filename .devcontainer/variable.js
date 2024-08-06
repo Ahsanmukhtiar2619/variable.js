df6b80444352b83103dee499a633bba957bb15d4
Ahsanmukhtiar2619 <ahsanmukhtiar447@gmail.com>
2024-08-06 07:48:36 +0000

    container config done

---
+++ .devcontainer/variable.js
@@ -0,0 +1,23 @@
+const  accountid = 122222
+//if we use const variable ,we cannot change the value of variable 
+
+let accountemail = "ahsanmukhtiar@643gmail.com"
+
+
+accountcity = "multan";
+
+//cannot use var because of issue block scope and functional scope
+
+accountcity ="lahore";
+
+accountemail = "ahssan@gmail.com"
+
+//accountid = 1222233445
+//accountid cannot be change beacuse of variable const
+ 
+
+let accountstate;
+
+var accountpassword = "1222348";
+
+console.table([accountid,,accountemail,accountstate,accountpassword])
