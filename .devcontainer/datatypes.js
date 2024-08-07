dc52360c262779be32dc769fdef883af55898625
Ahsanmukhtiar2619 <ahsanmukhtiar447@gmail.com>
2024-08-07 08:06:31 +0000

    container config done

---
+++ .devcontainer/datatypes.js
@@ -0,0 +1,35 @@
+"use strict";   // newer version use node js 
+
+// alert(59+11) we are using node js ,not browser
+
+
+let name ="ahsan"//string
+let age = 33 //number
+let isLoggedin = true; //boolean
+
+
+
+
+//Datatypes
+
+//premtive datatypes
+
+//number
+//null
+//boolean
+//bigint
+//string
+//symbol
+//undefined
+
+//non premptive datatypes
+
+//object
+
+console.log(typeof "");
+
+console.log(typeof null)
+
+console.log(typeof undefined)
+
+
