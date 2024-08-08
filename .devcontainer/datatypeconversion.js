ae5c7248daddb993f9314c31099f4e62fa4517a5
Ahsanmukhtiar2619 <ahsanmukhtiar447@gmail.com>
2024-08-08 15:31:29 +0000

    container config done

---
+++ .devcontainer/datatypeconversion.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+
+
+let test = "ahsan"
+
+let valueInstring = Number (test)
+console.log(typeof (valueInstring))
+
+
+let marks = 0;
+let marksInNumber = Boolean(marks)
+console.log(typeof (marksInNumber))
+console.log(marksInNumber)
+
+//33-> "33"
+//"33abc"->NaN
+//True->1
+//false->0
+//""->false
+//"ahsan"->true
