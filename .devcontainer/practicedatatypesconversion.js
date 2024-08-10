aca1b0430bfc25bb25937940aa8656be6c41a7ca
Ahsanmukhtiar2619 <ahsanmukhtiar447@gmail.com>
2024-08-10 05:33:47 +0000

    container config done

---
+++ .devcontainer/practicedatatypesconversion.js
@@ -0,0 +1,28 @@
+/*let test = "33aby"
+
+console.log(test)
+
+let valueInNumber = Number(test)
+
+console.log(typeof(valueInNumber))
+
+console.log(valueInNumber)
+
+let english = 1
+ 
+let valueInBoolean = Boolean(english)
+
+console.log(typeof (valueInBoolean))
+
+console.log(valueInBoolean)*/
+
+let math = null
+
+let valueInBoolean = Boolean(math)
+console.log(typeof(valueInBoolean))
+console.log(valueInBoolean)
+
+//run this code block by block
+//because same datattype are using
+//one block comment and one block run 
+//remmender
